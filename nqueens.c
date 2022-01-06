e0bfe240e3114e45d3f9ea1cdbe0464bb68d1088
camille-marro <>
2022-01-06 12:34:10 +0100

    fin de configuration de l'IDE + github

---
+++ nqueens.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <malloc.h>
+
+#include "list.h"
+#include "board.h"
+
+
+list_t openList_p;
+list_t closedList_p;
+
+
+void showSolution( Item *goal )
+{
+    int i = 0, j;
+
+    printf("\nSolution:");
+
+    while (goal) {
+        printBoard( goal );
+        goal = goal->parent;
+        i++;
+    }
+
+    printf("\nLength of the solution = %d\n", i-1);
+    printf("Size of open list = %d\n", openList_p.numElements);
+    printf("Size of closed list = %d\n", closedList_p.numElements);
+    return;
+}
+
+
+void bfs( void )
+{
+    Item *cur_node, *child_p, *temp;
+    int i;
+    while ( listCount(&openList_p) ) { /* While items are on the open list */
+
+        /* Get the first item on the open list */
+        cur_node = popFirst(&openList_p);
+
+        //printf("%d  %f\n", listCount(&openList_p), evaluateBoard( cur_node ));
+
+        /* Add it to the "visited" list */
+        addLast(&closedList_p, cur_node);
+
+        /* Do we have a solution? */
+        if ( evaluateBoard(cur_node) == 0.0 ) {
+            showSolution(cur_node);
+            return;
+
+        } else {
+
+            /* Enumerate adjacent states */
+            for (int i = 0; i < MAX_BOARD; i++) {
+                child_p = getChildBoard( cur_node, i );
+
+                if (child_p != NULL) { // it's a valid child!
+
+                    /* Ignore this child if already visited */
+
+                    /* Add child node to openList */
+                    addLast( &openList_p, child_p );
+                }
+            }
+        }
+    }
+}
+
+void dfs (void) {
+    Item *cur_node, *child_p, *tmp;
+    while (listCount(&openList_p)) {
+        cur_node = popFirst(&openList_p);
+
+        //printf("%d %f\n", listCount(&openList_p), evaluateBoard(cur_node));
+
+        addLast(&closedList_p, cur_node);
+
+        if (evaluateBoard(cur_node) == 0.0) {
+            showSolution(cur_node);
+            return;
+
+        } else {
+
+            for (int i = 0; i < MAX_BOARD; i++) {
+                child_p = getChildBoard( cur_node, i );
+
+                if (child_p != NULL) {
+                    addLast( &openList_p, child_p );
+                }
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    /* init lists */
+    initList(&openList_p);
+    initList(&closedList_p);
+
+
+    printf("\nInitial:");
+    Item *initial_state = initGame();
+    printBoard( initial_state );
+
+    // printf("\nSearching ...\n");
+    // addLast( &openList_p, initial_state );
+    // bfs();
+    // printf("Finished!\n");
+
+    printf("\nSearching ...\n");
+    addFirst (&openList_p, initial_state );
+    dfs();
+    printf("Finished!\n");
+
+    /* clean lists */
+    cleanupList( &openList_p );
+    cleanupList( &closedList_p );
+
+    return 0;
+}
